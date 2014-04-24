53c9b42489f0efc592958ca9707dd36783b590bf
Jakob Vesely <office@appartig.at>
2014-04-24 16:35:38 +0200

    Added Opacity Animation Option

--- jquery.countupcircle.js
+++ jquery.countupcircle.js
@@ -13,19 +13,25 @@
 	    **/
 
 		var settings = $.extend({
-			duration: 5000 //ms
+			duration: 5000, //ms
+			opacity_anim: false
 		}, options);
 
 		var toCount = parseInt(this.html());
-		console.log("Count up to " + toCount);
+		console.log("Count up to: " + toCount);
 
-		var i 	 = 0;
-		var step = settings.duration / toCount;
-		console.log("Step duration " + step);
+		var i 	 		 = 0;
+		var step 		 = settings.duration / toCount;
+		var procent_step = 1/toCount;
+		console.log("Step duration: " + step+"ms");
 
 		var displayNumber = function() {
 			i++;
 			self.html(i);
+			if (settings.opacity_anim){
+				console.log("animate opacity");
+				self.css({'opacity':procent_step*i});
+			}
 			if (i < toCount) {
 				setTimeout(displayNumber, step);
 			}
